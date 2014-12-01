7b89df35c09e329996e6d514e76fa74d815960d2
Florian Weber <florian.weber.dd@icloud.com>
2014-12-01 21:25:14 +0100

    domain entity

---
+++ src/itaw/DataBundle/Entity/Domain.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace itaw\DataBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Domain
+ *
+ * @ORM\Table("domain")
+ * @ORM\Entity
+ */
+class Domain
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="creationDate", type="datetime")
+     */
+    private $creationDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="editDate", type="datetime")
+     */
+    private $editDate;
+}
