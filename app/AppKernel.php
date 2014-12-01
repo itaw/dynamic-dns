6ba873e23f39188b3991ca6a10e264cfc424ac17
Florian Weber <florian.weber.dd@icloud.com>
2014-12-01 21:48:55 +0100

    fos user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new itaw\AppBundle\itawAppBundle(),
             new itaw\DnsBundle\itawDnsBundle(),
             new itaw\DataBundle\itawDataBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new \itaw\UserBundle\itawUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/itaw/UserBundle/itawUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace itaw\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class itawUserBundle extends Bundle
+{
+}
