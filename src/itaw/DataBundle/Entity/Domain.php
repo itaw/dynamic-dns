cda04f74885a365d81e2b3b5e19bcc2eb40fb5bc
Florian Weber <florian.weber.dd@icloud.com>
2014-12-01 21:27:23 +0100

    .

--- src/itaw/DataBundle/Entity/Domain.php
+++ src/itaw/DataBundle/Entity/Domain.php
@@ -41,4 +41,83 @@ class Domain
      * @ORM\Column(name="editDate", type="datetime")
      */
     private $editDate;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Domain
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set creationDate
+     *
+     * @param \DateTime $creationDate
+     * @return Domain
+     */
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get creationDate
+     *
+     * @return \DateTime 
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * Set editDate
+     *
+     * @param \DateTime $editDate
+     * @return Domain
+     */
+    public function setEditDate($editDate)
+    {
+        $this->editDate = $editDate;
+
+        return $this;
+    }
+
+    /**
+     * Get editDate
+     *
+     * @return \DateTime 
+     */
+    public function getEditDate()
+    {
+        return $this->editDate;
+    }
 }
