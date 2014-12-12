42a4383ba5c6f286367dc349c1bf082ea3471fbb
Florian Weber <florian.weber.dd@icloud.com>
2014-12-12 08:47:04 +0100

    pwd gen

--- src/itaw/Util/PasswordGenerator.php
+++ src/itaw/Util/PasswordGenerator.php
@@ -7,12 +7,13 @@ class PasswordGenerator
     /**
      * @return string
      */
-    public static function generate()
+    public static function generate($length = 16)
     {
-        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+        $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!§$%&/()=?-_.:,;";
         $pass = array(); //remember to declare $pass as an array
         $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-        for ($i = 0; $i < 16; $i++) {
+
+        for ($i = 0; $i < $length; $i++) {
             $n = rand(0, $alphaLength);
             $pass[] = $alphabet[$n];
         }
